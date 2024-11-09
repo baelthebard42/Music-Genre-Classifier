9d860f9cc7d8632bf1bce0c505452e3012e31c84
baelthebard42 <078bct012.anjal@pcampus.edu.np>
2024-11-09 12:59:23 +0545

    classifier with rnn-lstm

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 def load_data(path):
 
@@ -13,16 +14,41 @@ def load_data(path):
     return features, target
 
 
-def split_dataset(X, y, t_size, v_size):
+def split_dataset(X, y, t_size, v_size, for_CNN = False):
 
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=t_size)
 
    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=v_size)
    
-   X_train = X_train[..., np.newaxis]
-   X_val = X_val[..., np.newaxis]
-   X_test = X_test[..., np.newaxis]
+   if for_CNN:
+    X_train = X_train[..., np.newaxis]
+    X_val = X_val[..., np.newaxis]
+    X_test = X_test[..., np.newaxis]
    
    return X_train, X_val, X_test, y_train, y_val, y_test
+
+
+def plot_history(history):
+
+    fig, ax = plt.subplots(2)
+
+    # plotting accuracy
+
+    ax[0].plot(history.history["accuracy"], label="train_accuracy")
+    ax[0].plot(history.history["val_accuracy"], label="test_accuracy")
+    ax[0].set_ylabel("Accuracy")
+    ax[0].legend(fontsize = 5, loc="lower right")
+    ax[0].set_title("Accuracy")
+
+     # plotting error
+
+    ax[1].plot(history.history["loss"], label="train_error")
+    ax[1].plot(history.history["val_loss"], label="test_error")
+    ax[1].set_ylabel("Error")
+    ax[1].set_xlabel("Epoch")
+    ax[1].legend(fontsize = 5, loc= "upper right")
+    ax[1].set_title("Error")
+
+    plt.show()
 
 
